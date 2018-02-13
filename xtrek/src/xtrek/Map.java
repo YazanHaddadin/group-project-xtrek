9747c5e72a99db3b1208ed070c9888253e4fa1e2
Sebastien Michel <sebltm@gmail.com>
2018-02-13 12:17:24 +0000

    Created Map interface and implementation

---
+++ xtrek/src/xtrek/Map.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xtrek;
+/**
+ *
+ * @author sebltm
+ */
+public interface Map {
+    
+    /*byte[] readData(float latitude, float longitude, int zoom,
+            int sizeX, int sizeY);*/
+}
--- xtrek/src/xtrek/Xtrek.java
+++ xtrek/src/xtrek/Xtrek.java
@@ -5,17 +5,34 @@
  */
 package xtrek;
 
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import javax.swing.JFrame;
+
 /**
  *
  * @author sebltm
  */
-public class Xtrek {
+public class Xtrek extends JFrame {
 
     /**
      * @param args the command line arguments
      */
+    public void Xtrek() {
+        setTitle("XTrek");
+        setLayout(null); //GridBagLayout ?
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame xtrek = new Xtrek();
+        xtrek.setLocationRelativeTo(null);
+        
+        //Dimensions are in pixels, need to be mm
+        xtrek.setSize(new Dimension(103, 54));
+        
+        xtrek.setResizable(false);
+        xtrek.validate();
+        xtrek.setVisible(true);
     }
     
 }
