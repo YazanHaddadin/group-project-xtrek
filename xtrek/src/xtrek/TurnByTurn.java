4810b8f59d4923f00e9318650b3cd4dbb8caca09
Sebastien Michel <sebltm@gmail.com>
2018-02-16 11:06:12 +0000

    Set up connection to download voice messages

--- xtrek/src/xtrek/TurnByTurn.java
+++ xtrek/src/xtrek/TurnByTurn.java
@@ -13,6 +13,20 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import javax.net.ssl.HttpsURLConnection;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.SwingConstants;
@@ -24,12 +38,14 @@ import javax.swing.WindowConstants;
  */
 public class TurnByTurn extends JFrame implements Mode  {
     static private String language = "en-GB";
-    final JButton bOff = new LangButton("Off"     , null);
-    final JButton bEng = new LangButton("English" , "en-GB");
-    final JButton bFre = new LangButton("French"  , "fr-FR");
-    final JButton bGer = new LangButton("German"  , "de-DE");
-    final JButton bIta = new LangButton("Italian" , "it-IT");
-    final JButton bJap = new LangButton("Japanese", "ja-JP");
+    final JButton bOff = new LangButton("Off"     , null, null, null);
+    final JButton bEng = new LangButton("English" , "en-GB", "Male", "George");
+    final JButton bFre = new LangButton("French"  , "fr-FR", "Female", "Julie");
+    final JButton bGer = new LangButton("German"  , "de-DE", "Male", "Stefan");
+    final JButton bIta = new LangButton("Italian" , "it-IT", "Male", "Cosimo");
+    final JButton bJap = new LangButton("Japanese", "ja-JP", "Male", "EkaterinaRUS");
+    final static private String KEY1 = "10d30eade54847f881f88da8da8ac8ea";
+    final static private String KEY2 = "7277a9230ab04d8ea7f4ed2384077c25";
     
     public TurnByTurn() {
         displayMode();
@@ -62,11 +78,15 @@ public class TurnByTurn extends JFrame implements Mode  {
     
     class LangButton extends JButton implements SelectedListener {
         private String language;
+        private String name;
+        private String gender;
         private SelectButton select = new SelectButton();
         
-        public LangButton(String display, String language) {
+        public LangButton(String display, String language, String gender, String name) {
             super(display);
             this.language = language;
+            this.gender = gender;
+            this.name = name;
             setStyle();
             
             /*THE MOUSE LISTENER WILL GET REMOVED WHEN THE BUTTONS ARE PROPERLY
@@ -102,6 +122,112 @@ public class TurnByTurn extends JFrame implements Mode  {
             });
             
             select.setSelectedListener(this);
+        }
+        
+        private void getNextSegment(String segment, String KEY1) {
+            try {
+                String token = renewAccessToken(KEY1);
+                HashMap<String, String> requestProp = new HashMap<String, String>() {};
+                requestProp.put("Content-Type", "application/ssml+xml");
+                requestProp.put("X-Microsoft-OutputFormat", "riff-16khz-16bit-mono-pcm");
+                requestProp.put("Authorization", "Bearer"+token);
+                HttpsURLConnection conn = 
+                        setupConnection(
+                                "https://speech.platform.bing.com/synthesize", 
+                                "POST", 
+                                requestProp, 
+                                "<speak version='1.0' xml:lang='en-US'><voice xml:lang='"+language+"' xml:gender='"+gender+"' name='Microsoft Server Speech Text to Speech Voice ("+language+", "+name+")'>Microsoft Bing Voice Output API</voice></speak>");
+                
+                DataInputStream is = new DataInputStream(conn.getInputStream());
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                byte[] line = new byte[1000000]; //1MB
+                ByteArrayOutputStream response = new ByteArrayOutputStream(); 
+                while(true) {
+                  int n = is.read(line);
+                  if(n > 0) {
+                      response.write(line, 0, n);
+                  } else {
+                      break;
+                  }
+                }
+                rd.close();
+                conn.disconnect();
+            } catch (IOException e) {
+                //What do we do ?
+            }
+        }
+        
+        private String renewAccessToken(String key1) {
+            try {
+                HashMap<String, String> requestProp = new HashMap<String, String>() {};
+                requestProp.put("Ocp-Apim-Subscription-Key", key1);
+                HttpsURLConnection conn = 
+                        setupConnection(
+                                "https://api.cognitive.microsoft.com/sts/v1.0/issueToken", 
+                                "POST", 
+                                requestProp, 
+                                "");
+
+                //Get Response	
+                InputStream is = conn.getInputStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                String line;
+                StringBuffer response = new StringBuffer(); 
+                while((line = rd.readLine()) != null) {
+                  response.append(line);
+                }
+                rd.close();
+                conn.disconnect();
+                System.out.println(response.toString());
+                return response.toString();
+                
+                //return new String( response );
+            } catch (IOException e) {
+                //What do we do ?
+            }
+            
+            return null;
+          }
+        
+        private HttpsURLConnection setupConnection(String u, String method, Map<String, String> requestProp, String body) {
+            try {
+                URL url = new URL(u);
+                HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+                
+                conn.setRequestMethod(method);
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                
+                for(Map.Entry<String, String> e : requestProp.entrySet()) {
+                    String key = e.getKey();
+                    String value = e.getValue();
+                    conn.addRequestProperty(key, value);
+                }
+                
+                DataOutputStream wr = new DataOutputStream (conn.getOutputStream ());
+                wr.writeBytes(body);
+                wr.flush();
+                wr.close();
+                
+                return conn;
+            } catch (IOException e) {
+                //What do we do ?
+            }
+            
+            return null;
+        }
+        
+        private void writeData( byte[] buffer, String name ) {
+            try {
+                File             file = new File( name );
+                FileOutputStream fos  = new FileOutputStream( file );
+                DataOutputStream dos  = new DataOutputStream( fos ); 
+                dos.write( buffer );
+                dos.flush();
+                dos.close();
+            } catch ( Exception ex ) {
+                System.out.println( ex ); System.exit( 1 ); return;
+            }
         }
         
         private void focusGained() {
