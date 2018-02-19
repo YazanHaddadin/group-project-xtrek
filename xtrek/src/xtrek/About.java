0cb1722acf4861032390ca7dc892a735ba1d20b3
liamvinson <liamvinson1@gmail.com>
2018-02-19 20:55:54 +0000

    Formated to extend Mode

--- xtrek/src/xtrek/About.java
+++ xtrek/src/xtrek/About.java
@@ -12,12 +12,27 @@ import javax.swing.*;
  *
  * @author liamvinson
  */
-public class About  {
+public class About extends Mode  {
+
+    public About(JFrame frame) {
+        super(frame);
+    }
+    
+    @Override
+    public void displayMode() {
+
+        frame.setTitle("About");
+        
+        panel = new AboutPanel();
+        
+        panel.validate();
+        panel.setVisible(true);
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("FrameDemo");
         JPanel panel = new AboutPanel();
-        frame.setSize(new Dimension(220, 300));
+        frame.setSize(new Dimension(570, 710));
         frame.setResizable(false);
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
