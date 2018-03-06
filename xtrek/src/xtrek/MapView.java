93642a84fb4cc9733d53f0117f15204c7a7d10ea
Alex <Alex@10.173.65.85>
2018-03-06 11:25:31 +0000

    Map View in MVC

---
+++ xtrek/src/xtrek/MapView.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xtrek;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author Alex Vale
+ * @version Sprint 2
+ */
+public class MapView extends ModeView{
+    private Map controller;
+    private java.util.Timer timer;
+    private static JLabel label;
+    
+    public MapView(JFrame frame) {
+        super(frame);
+        displayMode();
+    }
+    
+    void setController(Map controller) {
+        this.controller = controller;
+    }
+    
+    @Override
+    public void makeVisible() {
+        panel.setVisible(true);
+        //MapModel.updateMap();
+    }
+
+    @Override
+    public void hide() {
+        panel.setVisible(false);
+        timer.cancel();
+    }
+    
+    @Override
+    public void displayMode() {
+        frame.setTitle("Map");
+
+        panel.setLayout(null);
+
+        label = new JLabel();
+
+        label.setBounds(0, 0, Constants.screenWidth, Constants.screenHeight);
+        panel.add(label);
+        
+        ImageIcon pic = new ImageIcon("redDot.jpg");
+        panel.add(new JLabel(pic));
+
+        panel.validate();
+        panel.setVisible(true);
+
+        timer = new java.util.Timer();
+    }
+    
+}
