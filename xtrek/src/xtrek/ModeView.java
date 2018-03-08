8fadeadc738ab8477b8991ff2c898c168b4993a1
Sebastien Michel <sebltm@gmail.com>
2018-03-08 15:51:18 +0000

    Removed overlay from ModeView

--- xtrek/src/xtrek/ModeView.java
+++ xtrek/src/xtrek/ModeView.java
@@ -23,16 +23,7 @@ public abstract class ModeView {
     ModeView(JFrame frame) {
         this.frame = frame;
         //Dimensions are in pixels, need to be mm
-        panel = new JPanel(){
-            public void paintComponent(Graphics g) {  
-                try{
-                    Image img = ImageIO.read(getClass().getResource("display.png"));
-                    g.drawImage(img, 0, 0, 600, 750, this);  
-                } catch(Exception ex){
-                    System.out.println(ex);
-                }
-            }
-        };        
+        panel = new JPanel();
         panel.setPreferredSize(new Dimension(Constants.screenWidth, Constants.screenHeight));
         panel.setMaximumSize(new Dimension(Constants.screenWidth, Constants.screenHeight));
         panel.setBackground(Color.BLACK);
