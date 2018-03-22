4663c109f16c6fa4dc699819fbaedfa0ba69c420
Alex <Alex@10.173.65.85>
2018-03-22 13:15:57 +0000

    rotation

--- xtrek/src/xtrek/MapView.java
+++ xtrek/src/xtrek/MapView.java
@@ -2,6 +2,7 @@ package xtrek;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * MapView class
@@ -13,7 +14,7 @@ import java.awt.*;
  */
 public class MapView extends ModeView {
     private static JLabel label = new JLabel();
-
+    private static BufferedImage image;
     MapView(JFrame frame) {
         super(frame);
         panel.setLayout(new GridBagLayout());
@@ -30,10 +31,19 @@ public class MapView extends ModeView {
         panel.setVisible(false);
     }
 
-    void setIcon(ImageIcon image) {
+    void setIcon(BufferedImage image) {
+        MapView.image = image;
         label.setIcon(image);
     }
-    
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.clearRect(0, 0, getWidth(), getHeight());
+        g2d.rotate( MapModel.getBearing(), image.getWidth()/2, image.getHeight()/2 );
+        g2d.drawImage( image, 0, 0, this);
+        
+    }
     @Override
     public void displayMode() {
         frame.setTitle("XTrek");
