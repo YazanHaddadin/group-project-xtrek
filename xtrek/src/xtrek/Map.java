5dee3d888e7a91775e9dcc95c91ffc1ba4794884
ASVale <as.vale@outlook.com>
2018-03-22 15:50:19 +0000

    comments

--- xtrek/src/xtrek/Map.java
+++ xtrek/src/xtrek/Map.java
@@ -99,22 +99,27 @@ public class Map extends Mode implements OnGPSUpdateListener {
     void setIcon(BufferedImage image) {
         mapView.setIcon(image);
     }
-
-    private final static double AVERAGE_RADIUS_OF_EARTH_KM = 6371;
-
+    //radius of earth in km
+    private final static double AVERAGE_RADIUS_OF_EARTH_KM = 6371; 
+    
+    
     static double calculateDistance(Double lat2, Double lon2, Double lat1, Double lon1) {
-
+        //calcuates the distance travelled by taking in the latitude and longitude of one point,
+        //and the latitude and longitude of the current point and comparing them.
         Double dlon = Math.toRadians(lon2 - lon1);
         Double dlat = Math.toRadians(lat2 - lat1);
+        //Math to make the distance match kilometres
         Double a = Math.pow(Math.sin(dlat / 2), 2) + (Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2));
         Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         return AVERAGE_RADIUS_OF_EARTH_KM * c;
     }
 
     @Override
+    
     public void onGPSUpdate(Double latitude, Double longitude,
                             SatelliteModel.Direction latitudeDirection,
                             SatelliteModel.Direction longitudeDirection) {
+        //makes changes once the GPS updates
         mapModel.onGPSUpdate(latitude, longitude, latitudeDirection, longitudeDirection);
     }
 }
